d0063bffe659707b397377adc5c800b28e09430f
EC2 Default User <ec2-user@ip-172-31-6-45.ap-northeast-1.compute.internal>
2021-03-27 04:13:58 +0000

    Q3を完了

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,26 @@
+@extends("leyouts")
+
+@section("content")
+
+    <div class="container mt-4">
+        <div class="card mt-4">
+            <div class="card-header mb-2">
+                Title
+            </div>
+            <div class="card-body">
+                <p class="card-text">
+                    body
+                </p>
+                
+            </div>
+            <div class="card-footer">
+                <span Class="mr-2">
+                    投稿日時
+                </span>
+            </div>
+            
+        </div>
+    </div>
+
+
+@endsection("content")
